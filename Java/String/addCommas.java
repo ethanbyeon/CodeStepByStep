635af148ca58d5029d49051f78196b3900243ff8
ethanbyeon <51931482+ethanbyeon@users.noreply.github.com>
2020-01-08 19:29:27 -0800

    Solved addCommas

---
+++ Java/String/addCommas.java
@@ -0,0 +1,27 @@
+package Java.String;
+
+class addCommas {
+
+    public static void main(String args[]) {
+        System.out.println(ac("12345678"));
+    }
+
+    static String ac(String x) {
+        String n = x;
+        String r = "";
+
+        while(true) {
+
+            if(n.length() <= 3) {
+                r = n + r;
+                break;
+            }
+
+            String temp = n.substring(n.length() - 3);
+            r = "," + temp + r;
+            n = n.substring(0, n.length() - 3);
+        }
+
+        return r;
+    }
+}
