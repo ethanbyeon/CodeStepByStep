a7495788da44081481a5e178dbeaf01ed4f97657
ethanbyeon <51931482+ethanbyeon@users.noreply.github.com>
2020-01-18 22:35:16 -0800

    solved reverseChunks

--- Java/String/reverseChunks.java
+++ Java/String/reverseChunks.java
@@ -3,23 +3,24 @@ package Java.String;
 class reverseChuncks {
 
     public static void main(String[] args) {
-        System.out.println(rchunks("Banana", 6));
+        System.out.println(rchunks("MehranSahami", 3));
     }
 
     static String rchunks(String s, int n) {
         
         String r = "";
 
+        if(s.length() == n) return s.substring(1) + s.charAt(0);
+        else if(n == 1) return s;
+
         String temp = "";
-        for(int i = 0; i < s.length() - n; i+=n){
+        for(int i = 0; i <= s.length() - n; i+=n){
             temp = s.substring(i, n + i);
             for(int j = temp.length() - 1; j >= 0; j--){
                 r += temp.charAt(j);
             }
         }
 
-        r += s.substring(s.length() - (s.length() % n));
-        
         return r;
     }
 
