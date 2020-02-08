ec35f7d45ad8edb140079634cdc2e7b91834464e
ethanbyeon <51931482+ethanbyeon@users.noreply.github.com>
2020-02-08 15:23:54 -0800

    fixed contains

--- Java/Arrays/contains.java
+++ Java/Arrays/contains.java
@@ -11,19 +11,19 @@ class contains {
 
     static boolean contains(int[] a1, int[] a2) {
 
-        if(a1.length < a2.length) return false;
-
-        int sec = 0;
-        for(int i = 0; i < a1.length; i++) { 
-            if(a1[i] == a2[sec]) {
-                sec++;
-                if (sec == a2.length) return true;
-            }else {
-                sec = 0;
-                i--;
+        for(int i = 0; i <= a1.length - a2.length; i++) {
+            boolean bool = true;
+            
+            for(int j = 0; j < a2.length; j++) {
+                if(a1[i + j] != a2[j]) {
+                    bool = false;
+                    break;
+                }
             }
+            
+            if(bool) return true;
         }
-
+        
         return false;
     }
 
